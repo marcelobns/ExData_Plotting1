666c1492c9bb2505b133a4d52c3953314e7964c5
marcelobns <marcelobns@live.com>
2015-07-12 14:07:55 -0400

    adjusts

--- plot1.R
+++ plot1.R
@@ -1,21 +1,18 @@
 plot1 <- function(){
     ## Reading data
-    consumptionData = read.table("household_power_consumption.txt", header=TRUE, sep=";")
+    consumption = read.table("household_power_consumption.txt", header=TRUE, sep=";", na="?", dec=".")
     
-    ## Selecting range by index
-    range = consumptionData[66637:69516, ]
+    ## Selecting range between date
+    range = consumption[consumption$Date %in% c("1/2/2007","2/2/2007") ,]
     
     ## Numeric convertion
-    range$Global_active_power = as.numeric(as.character(range$Global_active_power))
-    
-    #Range to dataframe
-    df = data.frame(range)
+    range$Global_active_power = as.numeric(range$Global_active_power)
     
     #Plot
-    hist(df$Global_active_power, main=paste("Global Active Power"), xlab="Global Active Power (kilowatts)", col="red")
+    hist(range$Global_active_power, main=paste("Global Active Power"), xlab="Global Active Power (kilowatts)", col="red")
     
     ##Save to png file 
     dev.copy(png, file = "plot1.png")
-    dev.off()
+    dev.off() 
 }
 plot1()
